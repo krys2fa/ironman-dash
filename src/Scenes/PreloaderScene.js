bf18171de1851c27812c4eb906207bb1b1c924ad
Christopher Amanor <kriskit7@gmail.com>
2020-09-10 07:21:21 +0000

    Reverting changes

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -79,7 +79,7 @@ export default class PreloaderScene extends Phaser.Scene {
     this.load.image('blueButton2', 'assets/game/blue_button03.png');
     this.load.image('box', 'assets/game/grey_box.png');
     this.load.image('checkedBox', 'assets/game/blue_boxCheckmark.png');
-    this.load.audio('bgMusic', ['assets/sounds/awesomeness.wav']);
+    this.load.audio('bgMusic', 'assets/sounds/awesomeness.wav');
   }
 
   init() {
