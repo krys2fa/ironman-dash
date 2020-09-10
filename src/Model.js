07e7b619a950fc8efef8b29119611d04f7fb5a88
Christopher Amanor <kriskit7@gmail.com>
2020-09-10 06:38:24 +0000

    Build project locally

--- src/Model.js
+++ src/Model.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 export default class Model {
   constructor() {
-    this._soundOn = true;
     this._musicOn = true;
     this._bgMusicPlaying = false;
   }
@@ -12,14 +11,6 @@ export default class Model {
 
   get musicOn() {
     return this._musicOn;
-  }
-
-  set soundOn(value) {
-    this._soundOn = value;
-  }
-
-  get soundOn() {
-    return this._soundOn;
   }
 
   set bgMusicPlaying(value) {
